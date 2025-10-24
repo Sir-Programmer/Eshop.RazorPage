9d2f228f03d1b14aa6124e5f9f3e0813996f6cdb
Mojtaba Karamlou <contact@karamlou.com>
2025-10-25 02:08:40 +0330

    Add SeoData class for managing SEO-related metadata

---
+++ Eshop.RazorPage/Models/SeoData.cs
@@ -0,0 +1,11 @@
+﻿namespace Eshop.RazorPage.Models;
+
+public class SeoData
+{
+    public string? MetaTitle { get; private set; }
+    public string? MetaDescription { get; private set; }
+    public string? MetaKeyWords { get; private set; }
+    public bool IndexPage { get; private set; }
+    public string? Canonical { get; private set; }
+    public string? Schema { get; private set; }
+}
