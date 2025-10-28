d140a2320b893e05ef0c82b07db3b3fb460e4a47
Mojtaba Karamlou <contact@karamlou.com>
2025-10-29 01:43:37 +0330

    Refactor AuthService to use module name in API endpoints for consistency

--- Eshop.RazorPage/Services/Auth/AuthService.cs
+++ Eshop.RazorPage/Services/Auth/AuthService.cs
@@ -6,46 +6,47 @@ namespace Eshop.RazorPage.Services.Auth;
 
 public class AuthService(HttpClient client, IHttpContextAccessor accessor) : IAuthService
 {
+    private const string ModuleName = "Auth";
     public async Task<ApiResult<LoginResponse>?> Login(LoginCommand command)
     {
-        var result = await client.PostAsJsonAsync("auth/login", command);
+        var result = await client.PostAsJsonAsync($"{ModuleName}/login", command);
         return await result.Content.ReadFromJsonAsync<ApiResult<LoginResponse>>();
     }
 
     public async Task<ApiResult?> RequestRegistration(RequestRegistrationCommand command)
     {
-        var result = await client.PostAsJsonAsync("auth/register", command);
+        var result = await client.PostAsJsonAsync($"{ModuleName}/register", command);
         return await result.Content.ReadFromJsonAsync<ApiResult>();
     }
 
     public async Task<ApiResult?> ConfirmRegistration(ConfirmRegistrationCommand command)
     {
-        var result = await client.PostAsJsonAsync("auth/register/confirm", command);
+        var result = await client.PostAsJsonAsync($"{ModuleName}/register/confirm", command);
         return await result.Content.ReadFromJsonAsync<ApiResult>();
     }
 
     public async Task<ApiResult?> RequestPasswordReset(RequestPasswordResetCommand command)
     {
-        var result = await client.PostAsJsonAsync("auth/password/request-reset", command);
+        var result = await client.PostAsJsonAsync($"{ModuleName}/password/request-reset", command);
         return await result.Content.ReadFromJsonAsync<ApiResult>();
     }
 
     public async Task<ApiResult?> ConfirmPasswordReset(ConfirmPasswordResetCommand command)
     {
-        var result = await client.PostAsJsonAsync("auth/password/confirm-reset", command);
+        var result = await client.PostAsJsonAsync($"{ModuleName}/password/confirm-reset", command);
         return await result.Content.ReadFromJsonAsync<ApiResult>();
     }
 
     public async Task<ApiResult<LoginResponse>?> RefreshToken()
     {
         var refreshToken = accessor.HttpContext?.Request.Cookies["refreshToken"];
-        var result = await client.PostAsync($"auth/refresh-token?refreshToken={refreshToken}", null);
+        var result = await client.PostAsync($"{ModuleName}/refresh-token?refreshToken={refreshToken}", null);
         return await result.Content.ReadFromJsonAsync<ApiResult<LoginResponse>>();
     }
 
     public async Task<ApiResult?> Logout()
     {
-        var result = await client.DeleteAsync($"auth/logout");
+        var result = await client.DeleteAsync($"{ModuleName}/logout");
         return await result.Content.ReadFromJsonAsync<ApiResult>();
     }
 }
